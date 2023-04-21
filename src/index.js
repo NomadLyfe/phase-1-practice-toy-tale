a73faf924571895c4a542b2403d47e4a3be96390
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-21 16:49:30 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -12,4 +12,68 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(data => createCards(data));
+
+  function createCards(toys) {
+    for ( const toy of toys) {
+      createCard(toy);
+    }
+  };
+
+  function createCard(toy) {
+    const card = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const btn = document.createElement('button');
+    card.className = 'card';
+    h2.textContent = toy.name;
+    img.src = toy.image;
+    img.className = 'toy-avatar';
+    p.textContent = `${toy.likes} Likes`;
+    btn.className = 'like-btn';
+    btn.textContent = 'Like ❤️';
+    btn.id = toy.id;
+    card.appendChild(h2);
+    card.appendChild(img);
+    card.appendChild(p);
+    card.appendChild(btn);
+    btn.addEventListener('click', function() {
+      toy.likes += 1;
+      p.textContent = `${toy.likes} Likes`;
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          'likes': toy.likes,
+        })
+      });
+    });
+    document.querySelector('#toy-collection').appendChild(card);
+  };
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': form.name.value,
+        'image': form.image.value,
+        'likes': 0
+      })
+    })
+      .then(resp => resp.json())
+      .then(data => createCard(data));
+  });
 });
